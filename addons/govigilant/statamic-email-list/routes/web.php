0c3dad7a76bc18fe7f28c93b207e605c0e69f314
Vincent Boon <vincent@justbetter.nl>
2024-06-01 10:59:51 +0200

    Signed url relative

--- addons/govigilant/statamic-email-list/routes/web.php
+++ addons/govigilant/statamic-email-list/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Vigilant\EmailList\Http\Controllers\MailListController;
 
 Route::get('confirm/{entry}', [MailListController::class, 'confirm'])
-    ->middleware('signed')
+    ->middleware('signed:relative')
     ->name('email-list.confirm');
