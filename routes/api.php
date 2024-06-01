dda90c95b33feb39d82506bd3f4c06da24ef7f37
Vincent Boon <vincent@justbetter.nl>
2024-06-01 10:57:13 +0200

    Adjust configuration

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/deploy', function(Request $request) {
+Route::post('/deploy', function (Request $request) {
 
     if (($signature = $request->headers->get('X-Hub-Signature')) == null) {
         abort(400, 'Signature header is not set.');
@@ -17,11 +17,11 @@ Route::post('/deploy', function(Request $request) {
 
     $webhookSignature = hash_hmac('sha1', $request->getContent(), config('services.deploy_key'));
 
-    if (!hash_equals($webhookSignature, $signatureData[1])) {
-        abort(401, 'Could not verify request signature ' . $signatureData[1]);
+    if (! hash_equals($webhookSignature, $signatureData[1])) {
+        abort(401, 'Could not verify request signature '.$signatureData[1]);
     }
 
-    exec("sh " . base_path() . "/deploy.sh &");
+    exec("sh ".base_path()."/deploy.sh &");
 
     return response('deploying');
 });
